008a1ee071a66012a2a523c0ad4d353620c8057f
Ravi Raghavan <ravi0308raghavan@gmail.com>
2023-09-21 09:40:44 -0400

    Got the rectangular rotation working

--- rr1133-mmh255/2d_rigid_body.py
+++ rr1133-mmh255/2d_rigid_body.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 polygons = np.load("rr1133-mmh255/2d_rigid_body.npy", allow_pickle= True)
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(dpi = 100)
+ax.set_aspect("equal")
 
 def generate_rectangle():
     center_point = np.array([np.random.uniform(0, 2), np.random.uniform(0, 2)])
@@ -14,26 +15,36 @@ def generate_rectangle():
     bottom_left = np.array([-1 * w/2, -1 * h/2])
     bottom_right = np.array([w/2, -1 * h/2])
 
-    angle = np.random.uniform(0, 2 * np.pi)
+    angle = np.deg2rad(np.random.uniform(0, 360))
     rotation_matrix = np.array([[np.cos(angle), -1 * np.sin(angle)], [np.sin(angle), np.cos(angle)]])
     
-    top_left = (top_left @ rotation_matrix.T) + center_point
-    top_right = (top_right @ rotation_matrix.T) + center_point
-    bottom_left = (bottom_left @ rotation_matrix.T) + center_point
-    bottom_right = (bottom_right @ rotation_matrix.T) + center_point
+    top_left = (rotation_matrix @ top_left.T).T + center_point
+    top_right = (rotation_matrix @ top_right.T).T + center_point
 
-    return [[bottom_right[0], top_right[0], top_left[0], bottom_left[0]], [bottom_right[1], top_right[1], top_left[1], bottom_left[1]]]
+    bottom_left = (rotation_matrix @ bottom_left.T).T + center_point
+    bottom_right = (rotation_matrix @ bottom_right.T).T + center_point
+    
+    return center_point, bottom_right, top_right, top_left, bottom_left
 
-rectangle_coordinates = generate_rectangle();
-bottom_right = [rectangle_coordinates[0][0], rectangle_coordinates[1][0]]
-bottom_left = [rectangle_coordinates[0][-1], rectangle_coordinates[1][-1]]
-top_right = [rectangle_coordinates[0][1], rectangle_coordinates[1][1]]
-top_left = [rectangle_coordinates[0][-2], rectangle_coordinates[1][-2]]
-
+center_point, bottom_right, top_right, top_left, bottom_left = generate_rectangle();
 print(bottom_right, top_right, top_left, bottom_left)
 
+print(np.linalg.norm(bottom_right - center_point) * 2)
+print(np.linalg.norm(top_right - center_point) * 2)
+print(np.linalg.norm(top_left - center_point) * 2)
+print(np.linalg.norm(bottom_left - center_point) * 2)
+
+print("------------------------------------------------")
+
+print(np.linalg.norm(bottom_right - top_right))
+print(np.linalg.norm(top_right - top_left))
+print(np.linalg.norm(top_left - bottom_left))
+print(np.linalg.norm(bottom_left - bottom_right))
+
+print("------------------------------------------------")
+
 slope1 = (top_right[1] - bottom_right[1]) / (top_right[0] - bottom_right[0])
-slope2 = (bottom_right[1] - bottom_left[1]) / (bottom_right[0] - bottom_left[0])
+slope2 = (bottom_left[1] - bottom_right[1]) / (bottom_left[0] - bottom_right[0])
 print(f"{slope1}, {slope2}, {slope1 * slope2}")
 
 slope1 = (top_left[1] - bottom_left[1]) / (top_left[0] - bottom_left[0])
@@ -48,8 +59,9 @@ slope1 = (top_left[1] - top_right[1]) / (top_left[0] - top_right[0])
 slope2 = (bottom_right[1] - top_right[1]) / (bottom_right[0] - top_right[0])
 print(f"{slope1}, {slope2}, {slope1 * slope2}")
 
+print("------------------------------------------------")
 
-ax.fill(rectangle_coordinates[0], rectangle_coordinates[1], alpha=.25, fc='red', ec='blue')
+ax.fill([vertex[0] for vertex in [bottom_right, top_right, top_left, bottom_left]], [vertex[1] for vertex in [bottom_right, top_right, top_left, bottom_left]], alpha=.25, fc='red', ec='blue')
 
 for index in range(len(polygons)):
     polygon = polygons[index]
---
+++ rr1133-mmh255/test.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import matplotlib
+
+f,ax = plt.subplots(dpi = 100)
+ax.set_aspect("equal")
+coords = [2.5,2] #center of rectangle 
+
+tr = matplotlib.transforms.Affine2D().rotate_deg_around(coords[0],coords[1], 45)
+ts = ax.transData
+t = tr + ts
+
+#Original Rectangle Patch
+rec0 = matplotlib.patches.Rectangle((1,1),3,2,linewidth=1,edgecolor='r',facecolor='none')
+ax.add_patch(rec0)
+
+#Rotated rectangle patch
+rect1 = matplotlib.patches.Rectangle((1,1),3,2,linewidth=1,edgecolor='b',facecolor='none',angle = 20, rotation_point = 'center')
+ax.add_patch(rect1)
+
+# Rectangles center
+plt.plot(coords[0],coords[1], marker='o', markersize=3, color="green")
+
+plt.grid(True)
+ax.set_xlim(0,6)
+ax.set_ylim(-1,4)
+
+plt.show()
