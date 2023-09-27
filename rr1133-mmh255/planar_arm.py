2c826c797338de737f4242e17d7b6e07acd738db
Michelle Han <105685887+mii-chelle@users.noreply.github.com>
2023-09-27 18:34:50 -0400

    working on occupancy grid rn

--- rr1133-mmh255/planar_arm.py
+++ rr1133-mmh255/planar_arm.py
@@ -1,22 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from collision_checking import*
-from matplotlib.patches import Polygon, Circle, Rectangle
-import math
+from matplotlib.patches import Polygon, Circle
 
-# create arm (2 rectangular polygons with 2 joints, unlimited rotation) 
 class PlanarArm:
-    # constructor
     def __init__(self, ax):
-        
-        #initalize joint angles to 0 and radius of joint to 0.05
+        # initalize joint angles 
         self.theta1, self.theta2, self.radius = 0,0, 0.05
 
-        # arm lengths and height:0.4m, 0.25m, 0.1m
+        # arm lengths and height
         self.L1, self.L2, self.W =  0.4, 0.25, 0.1
 
+        # keeps track of all parts of robot
+        self.robot = []
+
         # load polygonal scene
-        self.polygons = np.load('scene8.npy', allow_pickle= True)
+        self.polygons = np.load('arm_polygons.npy', allow_pickle = True)
+        self.start = True
         
         # set up the plot axis
         ax.set_xlim(0,2)
@@ -25,62 +25,83 @@ class PlanarArm:
         self.ax = ax
         self.fig = ax.figure
 
-        # set up environment, remove any polygonals colliding with arm
+        # set up environment with initial arm at (1,1) at 0 degrees
         self.plot_arm()
-
-        # removes polygons that collide with arm at start
-       #polygon_collisions = self.collision()
-       # if not polygon_collisions:
-       #     for collision in polygon_collisions:
-        #        self.polygons.remove(collision)
 
         # connect keyboard events to event handlers
         self.fig.canvas.mpl_connect('key_press_event', self.on_key_press)
 
-    # calculates positions and updates plot
+    # calculates position and orientation of arm
     def plot_arm(self):
+        self.ax.clear()
+        self.robot.clear()
+
         # inital position
         x0,y0 = 1,1
 
-        # calculate vertices, add 0.01 to the length to account for links (radius 0.05)
+        # calculate vertices, add 0.1 to the length to account for links (radius 0.05)
         x1 = x0 + (self.L1+.1) * np.cos(self.theta1) 
-        y1 = x0 + (self.L1+.1) * np.sin(self.theta1)
+        y1 = y0 + (self.L1+.1) * np.sin(self.theta1)
         x2 = x1 + (self.L2+.1) * np.cos(self.theta2 + self.theta1) 
         y2 = y1 + (self.L2+.1) * np.sin(self.theta2 + self.theta1) 
-
-        self.ax.clear()
 
         # generate arm joints
         self.generate_joint(self.radius, x0, y0)
         self.generate_joint(self.radius, x1, y1)
         self.generate_joint(self.radius, x2, y2)
 
-        # generate arm links, second arm depends on angle of first
-        box = np.array(self.generate_link((x0,y0),self.radius, self.theta1, self.L1, self.W))
-        box2 = np.array(self.generate_link((x1,y1),self.radius, self.theta2+self.theta1, self.L2, self.W))
+        # generate arm links
+        self.robot.append(np.array(self.get_link((x0,y0),self.radius, self.theta1, self.L1, self.W)))
+        self.robot.append(np.array(self.get_link((x1,y1),self.radius, self.theta2+self.theta1, self.L2, self.W)))
 
-        # generate bounding boxes for joints (???)
-        #square1 = Polygon(())
+        # generate bounding boxes for joints 
+        self.robot.append(np.array(self.get_link((x0,y0),-0.05, self.theta1, self.radius*2, self.radius*2)))
+        self.robot.append(np.array(self.get_link((x1,y1), -0.05, self.theta2, self.radius*2, self.radius*2)))
+        self.robot.append(np.array(self.get_link((x2,y2), -0.05, self.theta2, self.radius*2, self.radius*2)))
 
-        # check for collisions
-        self.collision(box)
-        self.collision(box2)
-        # if not empty, revert to previous move
+        # adds links to plot
+        self.generate_link(self.robot[0], False)
+        self.generate_link(self.robot[1], False)
+        self.generate_link(self.robot[2], True)
+        self.generate_link(self.robot[3], True)
+        self.generate_link(self.robot[4], True)
 
+        # draw 
         self.plot_polygons()
         self.ax.set_xlim(0,2)
         self.ax.set_ylim(0,2)
-
-        #redraw
         self.fig.canvas.draw()
 
-    # generate joints using radius and a given point
+    # plots polygonal map
+    def plot_polygons(self):
+        # removes any polygons colliding with arm at START ENVIRONMENT 
+        if self.start:
+            for part in self.robot:
+                self.start_collision(part)   
+            self.start = False
+
+        # adds polygons to map
+        for polygon in self.polygons:
+             self.ax.add_patch(Polygon(polygon, closed = True, ec = 'black',facecolor = 'grey', alpha = 0.3))
+
+        #redraw
+        self.fig.canvas.draw()  
+
+    # generate a joint and add it to the plot
     def generate_joint(self, radius, x, y):
         circle = Circle((x,y),radius, fill = True, ec = 'blue', color = 'cornflowerblue')
-        ax.add_patch(circle)
+        self.ax.add_patch(circle)
 
-    # returns a rectangle ->represent rotating around a T
-    def generate_link(self, joint, radius,  angle, length, width):
+    # generate a link (doesn't add to plot if it is a joint box)
+    def generate_link(self, rectangle, boundBox):
+        if boundBox:
+            arm1 = Polygon(rectangle)
+        else:
+            arm1 = Polygon(rectangle, closed = True, ec = 'black', facecolor = 'cornflowerblue', alpha =0.5 )
+            self.ax.add_patch(arm1)
+
+    # returns a rectangle -> represent rotating around a T
+    def get_link(self, joint, radius,  angle, length, width):
         # initialize rectangle points (increment by radius to account for joint)
         LL = (joint[0]+radius, joint[1]-width/2)
         LR = (joint[0]+radius+length, joint[1]-width/2)
@@ -105,32 +126,45 @@ class PlanarArm:
         LR = (LR[0]+M[0], LR[1]+M[1])
         TR = (TR[0]+M[0], TR[1]+M[1])
         TL = (TL[0]+M[0], TL[1]+M[1])
-
         rectangle = [LL,LR,TR,TL, LL]
-        arm1 = Polygon(rectangle, closed = True, ec = 'black', facecolor = 'cornflowerblue', alpha =0.5 )
-        ax.add_patch(arm1)
-        return rectangle
 
-    # plots polygonal map
-    def plot_polygons(self):
-        for polygon in self.polygons:
-             ax.add_patch(Polygon(polygon, closed = True, ec = 'black',facecolor = 'grey', alpha = 0.3))
-        #redraw
-        self.fig.canvas.draw()   
+        return rectangle 
 
-    # checks if bounding box collides with polygons
-    def collision(self, box):
-        obstacle = []
-        for polygon in self.polygons:
-            if collides(box, polygon):
-                obstacle.append(polygon)
-                print("Collision Detected: Previous position restored")
-        return obstacle
+    # checks for any start collisions and removes them
+    def start_collision(self, box):
+        new_polygons = self.polygons
+        for i, polygon in enumerate(self.polygons):
+            if collides_optimized(box, polygon):
+                if self.start :
+                    new_polygons = np.delete(self.polygons, i, 0)
+        self.polygons =  new_polygons
 
-    # responds press to key events, if a key causes a collision, it undos the step (how to access rectangles and circles???????)
+    # checks if polygon collision is detected (if yes, revert robot)
+    def check_collisions(self, prev_theta1, prev_theta2):
+        # constructs new arm after key event
+        self.plot_arm()
+
+        # checks if any part of arm collides with polygon
+        collision_detected = False
+        for part in self.robot:
+            for polygon in self.polygons:
+                if collides_optimized(part, polygon):
+                    collision_detected = True
+                    break
+        
+        # reverts angles and generates previous arm
+        if collision_detected:
+            self.theta1 = prev_theta1
+            self.theta2 = prev_theta2
+            self.plot_arm() 
+
     def on_key_press(self, event):
-        # angle step
-        step = np.pi/30
+        # hold previous joint angles
+        prev_theta1 = self.theta1
+        prev_theta2 = self.theta2
+
+        step = np.pi/30 #angle step
+        self.event = event.key
         if event.key == 'up':
             self.theta1 += step
         elif event.key == 'down':
@@ -139,7 +173,9 @@ class PlanarArm:
             self.theta2 += step
         elif event.key == 'left':
             self.theta2 -= step
-        self.plot_arm()
+
+        # check for collisions 
+        self.check_collisions(prev_theta1, prev_theta2)
 
 # display plot
 fig, ax = plt.subplots()
