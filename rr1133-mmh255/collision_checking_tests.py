cdef6003436bc67cbe9837012d0aa4d9b74e0a29
Ravi Raghavan <ravi0308raghavan@gmail.com>
2023-09-28 17:57:44 -0400

    Re-generated Problem 2 Scene Files

--- rr1133-mmh255/collision_checking_tests.py
+++ rr1133-mmh255/collision_checking_tests.py
@@ -7,5 +7,5 @@ scenes = ["scene.npy", "scene2.npy", "scene3.npy", "scene4.npy", "scene5.npy", "
 
 for index, scene in enumerate(scenes):
     polygons = np.load(f"rr1133-mmh255/{scene}", allow_pickle = True)
-    file_name = f"rr1133-mmh255/Problem2_scene{index}.jpg"
+    file_name = f"rr1133-mmh255/Problem2_scene{index + 1}.jpg"
     plot(polygons, file_name, False)
