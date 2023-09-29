a45a0bddab1da5cb3ca4a7718da69b60bb3c4d61
Ravi Raghavan <ravi0308raghavan@gmail.com>
2023-09-29 00:11:31 -0400

    Made some changes

--- rr1133-mmh255/collision_checking_tests.py
+++ rr1133-mmh255/collision_checking_tests.py
@@ -4,13 +4,9 @@ from collision_checking import *
 
 #Testing this on multiple polygon scenes
 scenes = ["scene.npy", "scene2.npy", "scene3.npy", "scene4.npy", "scene5.npy", "scene6.npy", "scene7.npy", "scene8.npy"]
-scenes = ["scene.npy", "scene2.npy", "scene3.npy", "scene4.npy", "scene5.npy", "scene6.npy", "scene7.npy"]
+
 for index, scene in enumerate(scenes):
-    if index < 6: continue
     polygons = np.load(f"rr1133-mmh255/{scene}", allow_pickle = True)
     file_name = f"rr1133-mmh255/Problem2_scene{index + 1}.jpg"
-    print_diagnostics = False
-    if (index == 1):
-        print_diagnostics = True
-        
+    print_diagnostics = False        
     plot(polygons, file_name, False, print_diagnostics)
